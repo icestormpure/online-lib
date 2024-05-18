ebef1cd2f4805705fe46fd737a4a80679309c194
Afiq Aizzat <116041553+icestormpure@users.noreply.github.com>
2024-05-18 23:25:21 +0800

    Add files via upload

--- check_availability.php
+++ check_availability.php
@@ -1,30 +1,16 @@
 <?php 
 require_once("includes/config.php");
-// code user email availablity
-if(!empty($_POST["emailid"])) {
-	$email= $_POST["emailid"];
-	if (filter_var($email, FILTER_VALIDATE_EMAIL)===false) {
-
-		echo "error : You did not enter a valid email.";
-	}
-	else {
-		$sql ="SELECT EmailId FROM tblstudents WHERE EmailId=:email";
+//code check email
+if(!empty($_POST["isbn"])) {
+$isbn=$_POST["isbn"];
+$sql ="SELECT id FROM tblbooks WHERE ISBNNumber=:isbn";
 $query= $dbh -> prepare($sql);
-$query-> bindParam(':email', $email, PDO::PARAM_STR);
+$query-> bindParam(':isbn', $isbn, PDO::PARAM_STR);
 $query-> execute();
 $results = $query -> fetchAll(PDO::FETCH_OBJ);
-$cnt=1;
-if($query -> rowCount() > 0)
-{
-echo "<span style='color:red'> Email already exists .</span>";
- echo "<script>$('#submit').prop('disabled',true);</script>";
-} else{
-	
-	echo "<span style='color:green'> Email available for Registration .</span>";
- echo "<script>$('#submit').prop('disabled',false);</script>";
+ 
+if($query -> rowCount() > 0){
+echo "<span style='color:red'> ISBN already exists with another book. .</span>"; 
+echo "<script>$('#add').prop('disabled',true);</script>";
+} else { echo "<script>$('#add').prop('disabled',false);</script>";}
 }
-}
-}
-
-
-?>
--- dashboard.php
+++ dashboard.php
@@ -2,7 +2,7 @@
 session_start();
 error_reporting(0);
 include('includes/config.php');
-if(strlen($_SESSION['login'])==0)
+if(strlen($_SESSION['alogin'])==0)
   { 
 header('location:index.php');
 }
@@ -14,7 +14,10 @@ else{?>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Online Library Management System | User Dash Board</title>
+    <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <![endif]-->
+    <title>Online Library Management System | Admin Dash Board</title>
     <!-- BOOTSTRAP CORE STYLE  -->
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
     <!-- FONT AWESOME STYLE  -->
@@ -33,17 +36,15 @@ else{?>
          <div class="container">
         <div class="row pad-botm">
             <div class="col-md-12">
-                <h4 class="header-line">User DASHBOARD</h4>
+                <h4 class="header-line">ADMIN DASHBOARD</h4>
                 
                             </div>
 
         </div>
              
              <div class="row">
-
-
-<a href="listed-books.php">
-<div class="col-md-4 col-sm-4 col-xs-6">
+<a href="manage-books.php">
+ <div class="col-md-3 col-sm-3 col-xs-6">
  <div class="alert alert-success back-widget-set text-center">
  <i class="fa fa-book fa-5x"></i>
 <?php 
@@ -56,17 +57,16 @@ $listdbooks=$query->rowCount();
 <h3><?php echo htmlentities($listdbooks);?></h3>
 Books Listed
 </div></div></a>
-             
-               <div class="col-md-4 col-sm-4 col-xs-6">
+
+            
+       
+             <a href="manage-issued-books.php">
+               <div class="col-md-3 col-sm-3 col-xs-6">
                       <div class="alert alert-warning back-widget-set text-center">
                             <i class="fa fa-recycle fa-5x"></i>
 <?php 
-$rsts=0;
- $sid=$_SESSION['stdid'];
-$sql2 ="SELECT id from tblissuedbookdetails where StudentID=:sid and (RetrunStatus=:rsts || RetrunStatus is null || RetrunStatus='')";
+$sql2 ="SELECT id from tblissuedbookdetails where (RetrunStatus='' || RetrunStatus is null)";
 $query2 = $dbh -> prepare($sql2);
-$query2->bindParam(':sid',$sid,PDO::PARAM_STR);
-$query2->bindParam(':rsts',$rsts,PDO::PARAM_STR);
 $query2->execute();
 $results2=$query2->fetchAll(PDO::FETCH_OBJ);
 $returnedbooks=$query2->rowCount();
@@ -76,20 +76,68 @@ $returnedbooks=$query2->rowCount();
                           Books Not Returned Yet
                         </div>
                     </div>
+                </a>
 
-<a href="issued-books.php">
-<div class="col-md-4 col-sm-4 col-xs-6">
- <div class="alert alert-success back-widget-set text-center">
- <i class="fa fa-book fa-5x"></i>
-      <h3>&nbsp;</h3>
-Issued Books
-</div></div></a>
+  <a href="reg-students.php">
+               <div class="col-md-3 col-sm-3 col-xs-6">
+                      <div class="alert alert-danger back-widget-set text-center">
+                            <i class="fa fa-users fa-5x"></i>
+                            <?php 
+$sql3 ="SELECT id from tblstudents ";
+$query3 = $dbh -> prepare($sql3);
+$query3->execute();
+$results3=$query3->fetchAll(PDO::FETCH_OBJ);
+$regstds=$query3->rowCount();
+?>
+                            <h3><?php echo htmlentities($regstds);?></h3>
+                           Registered Users
+                        </div>
+                    </div></a>
+
+
+  <a href="manage-authors.php">
+ <div class="col-md-3 col-sm-3 col-xs-6">
+                      <div class="alert alert-success back-widget-set text-center">
+                            <i class="fa fa-user fa-5x"></i>
+<?php 
+$sq4 ="SELECT id from tblauthors ";
+$query4 = $dbh -> prepare($sq4);
+$query4->execute();
+$results4=$query4->fetchAll(PDO::FETCH_OBJ);
+$listdathrs=$query4->rowCount();
+?>
+<h3><?php echo htmlentities($listdathrs);?></h3>
+Authors Listed
+</div>
+</div></a>
+</div>
 
 
 
+ <div class="row">
 
 
-        </div>    
+
+  <a href="manage-categories.php">            
+<div class="col-md-3 col-sm-3 rscol-xs-6">
+<div class="alert alert-info back-widget-set text-center">
+<i class="fa fa-file-archive-o fa-5x"></i>
+<?php 
+$sql5 ="SELECT id from tblcategory ";
+$query5 = $dbh -> prepare($sql5);
+$query5->execute();
+$results5=$query5->fetchAll(PDO::FETCH_OBJ);
+$listdcats=$query5->rowCount();
+?>
+
+                            <h3><?php echo htmlentities($listdcats);?> </h3>
+                           Listed Categories
+                        </div>
+                    </div></a>
+             
+
+        </div>             
+            
     </div>
     </div>
      <!-- CONTENT-WRAPPER SECTION END-->
